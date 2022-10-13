d9ad94b4a25f98222073e57f6756725c0c67a2a7
MikeyMike7777 <michaelm246810@gmail.com>
2022-10-13 16:29:38 -0500

    Phone number formatter

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,5 +1,5 @@
 class TestClass {
-    public static void main (String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
