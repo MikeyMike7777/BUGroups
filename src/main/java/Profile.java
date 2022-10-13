a21e5cd94ca1938b3d67e76d9874591a666f7b63
MikeyMike7777 <michaelm246810@gmail.com>
2022-10-13 17:21:23 -0500

    Phone number methods

--- src/main/java/Profile.java
+++ src/main/java/Profile.java
@@ -4,8 +4,28 @@ public class Profile {
     private String description;
     private String phoneNumber;
 
-    public String formatPhone(String input) {
+
+    /*
+     * name: formatPhoneNumber
+     * parameters: input phone number (numbers only)
+     * return: formatted phone number
+     * description: changes phone number from 10 numbers to (XXX) XXX-XXXX
+     */
+    public String formatPhoneNumber(String input) {
         return input.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
+    }
+    /*
+     * name: verifyPhoneNumber
+     * parameters: input phone number (numbers only)
+     * return: true if phone number is valid
+     * description: makes sure a phone number is valid
+     */
+    public boolean verfiyPhoneNumber(String input){
+        boolean valid = true;
+        if(input.length() < 10 || input.length() > 13){
+            valid = false;
+        }
+        return valid;
     }
 
     public String getName() {
