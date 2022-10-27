a036467d73db791a1ba51e94d5654c9f71ebf993
Gabriel Goulis <ninjagabe2003@gmail.com>
2022-10-27 18:05:14 -0500

    Sizing changes in BUGroups.java

--- src/main/java/BUGroups.java
+++ src/main/java/BUGroups.java
@@ -4,19 +4,22 @@ import javax.swing.*;
 import java.awt.*;
 
 class BUGroups {
+    final static int APP_WIDTH = 800;
+    final static int APP_HEIGHT = 600;
+
     private static void createAndShowGUI() {
-        JFrame frame = new JFrame("BU Groups");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(600, 400));
+        JFrame app = new JFrame("BU Groups");
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setPreferredSize(new Dimension(APP_WIDTH, APP_HEIGHT));
 
         JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(600, 400));
+        panel.setPreferredSize(app.getPreferredSize());
         panel.add(new Login(panel.getPreferredSize()));
         panel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        frame.add(panel); // comment out when testing other pages
+        app.add(panel); // comment out when testing other pages
 
-        frame.pack();
-        frame.setVisible(true);
+        app.pack();
+        app.setVisible(true);
     }
 
     public static void main(String[] args) {
