e48ba4d9b93ac939ca6fd2e64a4ca6737d77ccf9
Bryce Robinson <bryce_robinson37@yahoo.com>
2022-12-01 15:26:34 -0600

    Implemented Message methods

--- src/main/java/message/Message.java
+++ src/main/java/message/Message.java
@@ -1,12 +1,29 @@
 package message;
 
+import student.Student;
+
+import java.util.*;
+
 public class Message {
     private String text;
     private String author;
-    private String time; // do we want to represent as a string? format?
+    private String time;
     private String courseNumber;
+    private Collection<Message> replies = new Vector<>();
+    private MessageBoard board;
+    private Student student;
 
-    public void replyToMessage(){
-        // relationship to createMessage in MessageBoard class?
+    Message(String text, String author, String time, String courseNumber,
+            MessageBoard board, Student student) {
+        this.text = text;
+        this.author = author;
+        this.time = time;
+        this.courseNumber = courseNumber;
+        this.board = board;
+        this.student = student;
+    }
+
+    public void replyToMessage(Message message) {
+        message.replies.add(this);
     }
 }
