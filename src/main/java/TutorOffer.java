0d54c183153201db0bea7a1f77975291f2ad0117
Bryce Robinson <bryce_robinson37@yahoo.com>
2022-12-01 15:08:18 -0600

    Implemented TutorOffer constructor

--- src/main/java/TutorOffer.java
+++ src/main/java/TutorOffer.java
@@ -1,16 +1,15 @@
 public class TutorOffer {
     private Course course;
     private String professorTaken;
-    private String semesterTaken; // how will this be formatted? e.g. S2021 vs Spring 2021 vs Spring 21 etc.
+    private String semesterTaken;
     private Double hourlyRate;
 
-    // do we want a parameterized constructor instead?
-    public void addTutoringOffer(Course course, String proferssorTaken,
+
+    TutorOffer(Course course, String professorTaken,
                                  String semesterTaken, Double hourlyRate){
-
-    }
-
-    public void removeTutoringOffer(){
-
+        this.course = course;
+        this.professorTaken = professorTaken;
+        this.semesterTaken = semesterTaken;
+        this.hourlyRate = hourlyRate;
     }
 }
