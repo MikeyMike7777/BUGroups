e986f0fb8e4d773f350d3f1f4f6655b469e5eb78
Bryce Robinson <bryce_robinson37@yahoo.com>
2022-12-01 15:04:01 -0600

    Implemented Course constructor

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -2,4 +2,10 @@ public class Course {
     private String professor;
     private Integer section;
     private String courseCode;
+
+    Course(String professor, Integer section, String courseCode) {
+        this.professor = professor;
+        this.section = section;
+        this.courseCode = courseCode;
+    }
 }
