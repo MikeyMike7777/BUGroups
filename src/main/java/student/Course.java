85ed8144b970e259340308deb1ae3ba201990ee5
CarsonBuntin <carson_buntin1@baylor.edu>
2022-12-02 16:30:46 -0600

    Profile page things

--- src/main/java/student/Course.java
+++ src/main/java/student/Course.java
@@ -10,4 +10,28 @@ public class Course {
         this.section = section;
         this.courseCode = courseCode;
     }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public Integer getSection() {
+        return section;
+    }
+
+    public void setSection(Integer section) {
+        this.section = section;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 }
--- src/main/java/student/StudentService.java
+++ src/main/java/student/StudentService.java
@@ -1,4 +1,13 @@
 package student;
 
 public class StudentService {
+
+    CourseDAO courseDAO = new CourseDAO();
+
+
+    public void createCourse(String professor, Integer section, String courseCode){
+        courseDAO.createCourse(professor, section, courseCode);
+    }
+
+
 }
