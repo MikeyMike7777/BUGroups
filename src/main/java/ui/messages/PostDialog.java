a2f826cf5f979fbb5fdbf3317896c036b2a0eded
Bryce Robinson <bryce_robinson37@yahoo.com>
2022-12-07 02:37:26 -0600

    Real messages fix

--- src/main/java/ui/messages/PostDialog.java
+++ src/main/java/ui/messages/PostDialog.java
@@ -98,8 +98,8 @@ public class PostDialog extends JDialog {
                 if (message.getText().length() < 1024) {
                     String temp = course.getText().toUpperCase();
                     String course;
-                    if (temp.charAt(4) != ' ' && temp.charAt(5) != ' ')
-                        course = temp.replaceAll(" ", "");
+                    if (temp.charAt(3) != ' ' && temp.charAt(4) != ' ')
+                        course = temp.split("[1-4][1-5][0-9]{2}")[0] + ' ' + temp.split("[a-zA-Z]{3,4}")[1];
                     else course = PostDialog.this.course.getText();
                     BUGUtils.controller.createMessage(
                             message.getText(), Window.username, course,
